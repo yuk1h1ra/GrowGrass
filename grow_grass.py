0c35b9557e4539f363a53ad1303c77d34a95cabf
yuk1h1ra <koheishoji913@gmail.com>
2017-11-13 09:28:34 +0900

    script commit and push

--- grow_grass.py
+++ grow_grass.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
 
 f = open('grasses.txt', 'a')
 f.write('w')
 f.close
 
-
+os.system('git add grasses.txt')
+os.system('git commit -m "w"')
+os.system('git push origin master')
